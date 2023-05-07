3db7e608c902dbb605331ddd344042ff72c0a370
PhucMaii <maithienphuc0102@gmail.com>
2023-05-06 23:24:30 -0700

    complete pagination

--- public/js/shop.js
+++ public/js/shop.js
@@ -10,8 +10,9 @@ const getUserInfo = () => {
   }
 
 // get all clothes
-const itemPerPage = 9;
-const currentPage = 1;
+let itemPerPage = 9;
+let currentPage = 1;
+
 const fetchAllClothes = async (page) => {
     const response = await fetch(`${baseUrl}/clothes?page=${page}&limit=${itemPerPage}`, {
         method: "GET",
@@ -21,8 +22,19 @@ const fetchAllClothes = async (page) => {
     })
     const clothesData = await response.json();
     const data = clothesData.data;
-    console.log(data);
+
+    return data;
+
+
+}
+
+// Call fetchAllClothes function whenever reload the page
+window.onload = async () => {
+    const data = await fetchAllClothes(currentPage);
+    
+    totalPages = data.totalPages;
     const clothesCardContainer = document.querySelector('#clothes-card-container');
+    clothesCardContainer.innerHTML = '';
     for(let item of data.docs) {
         clothesCardContainer.innerHTML += `<div class="clothes-card"  >
         <div class="image-background" style="background-image: url(${item.img})"></div>
@@ -36,8 +48,50 @@ const fetchAllClothes = async (page) => {
     }
 }
 
+// Call fetchAllClothes function whenever user navigate to other page
+const goToPage = async (page) => {
+    currentPage = page;
+    const data = await fetchAllClothes(currentPage);
+    const clothesCardContainer = document.querySelector('#clothes-card-container');
+    clothesCardContainer.innerHTML = '';
+    for(let item of data.docs) {
+        clothesCardContainer.innerHTML += `<div class="clothes-card"  >
+        <div class="image-background" style="background-image: url(${item.img})"></div>
+        <div class="clothes-type description">
+            ${item.name}
+        </div>
+        <div class="clothes-price description">
+            ${item.price} VND
+        </div>
+    </div>`
+    }
+}
+
+// Add goToPage function to each of the page navigator
+const pagination = async () => {
+    const response = await fetch(`${baseUrl}/clothes`, {
+        method: "GET",
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+    const clothesData = await response.json();
+    const data = clothesData.data;
+
+    const paginationControls = document.querySelector('#pagination-controls');
+    for (let i = 1; i <= totalPages; i++) {
+        console.log('hi');
+        const button = document.createElement('button');
+        button.classList.add('page-button');
+        button.textContent = i;
+        button.onclick = () => goToPage(i);
+        paginationControls.appendChild(button);
+    }
+
+}
+pagination();
+
+
 const homeNav = () => {
     window.location.href = `${baseUrl}/customers/dashboard`;
 }
-
-fetchAllClothes(1);
