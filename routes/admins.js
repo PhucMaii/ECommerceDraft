b6f7be4c1027a41e670f0e05c62dec05b9ace92c
PhucMaii <maithienphuc0102@gmail.com>
2023-05-20 20:28:24 -0700

    delete admin

--- routes/admins.js
+++ routes/admins.js
@@ -5,22 +5,22 @@ const AdminController = require('../controllers/admins')
 
 router.use(express.static(path.join(__dirname, "../public")));
 // HomePage
-router.get('/dashboard', (req, res) => {
-    const filePath = path.join(__dirname, '..', 'public', 'html', 'admin' , 'dashboard.html');
-    res.sendFile(filePath);
-});
+// router.get('/dashboard', (req, res) => {
+//     const filePath = path.join(__dirname, '..', 'public', 'html', 'admin' , 'dashboard.html');
+//     res.sendFile(filePath);
+// });
 
-// Get signup page
-router.get('/signup', (req, res) => {
-    const filePath = path.join(__dirname, '..', 'public', 'html', 'admin' ,  'adminSignup.html');
-    res.sendFile(filePath);
-})
+// // Get signup page
+// router.get('/signup', (req, res) => {
+//     const filePath = path.join(__dirname, '..', 'public', 'html', 'admin' ,  'adminSignup.html');
+//     res.sendFile(filePath);
+// })
 
-// Get login page
-router.get('/login', (req, res) => {
-    const filePath = path.join(__dirname, '..', 'public', 'html', 'admin', 'adminLogin.html');
-    res.sendFile(filePath);
-})
+// // Get login page
+// router.get('/login', (req, res) => {
+//     const filePath = path.join(__dirname, '..', 'public', 'html', 'admin', 'adminLogin.html');
+//     res.sendFile(filePath);
+// })
 
 // API for admins
 // admin Sign Up
