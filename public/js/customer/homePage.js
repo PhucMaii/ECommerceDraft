d7da499a943572930351e193427c7277b2db05a3
PhucMaii <118595247+PhucMaii@users.noreply.github.com>
2023-05-24 09:22:41 -0700

    change baseUrl

--- public/js/customer/homePage.js
+++ public/js/customer/homePage.js
@@ -8,8 +8,8 @@ function myFunction() {
   }
 }
 // FETCH API
-const base = "https://ecommerce-r7tm.onrender.com";
-const baseUrl = `${base}/api/v1`;
+//const base = "https://ecommerce-r7tm.onrender.com";
+const baseUrl = `/api/v1`;
 
 const getUserInfo = () => {
   const userInfo = JSON.parse(localStorage.getItem('current-user'));
